3c7135c087770efb3e0cceaf2d1d8d3a8e16927f
iruy <yuri_matteus@hotmail.com>
2025-06-09 13:58:24 -0300

    create: button "fale com nossos especialistas"

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, NgOptimizedImage],
+  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
